98ee9572ba6bdaf2e84c8b96982bb0352a4f99b8
Nick Burnett <nicolasandrewburnett@gmail.com>
2025-03-14 13:21:11 -0400

    save

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sh -c "$(curl -fsLS get.chezmoi.io)"
 mv /home/bits/bin/chezmoi /home/bits/.local/bin
 chezmoi init https://github.com/NickBurnett/chezmoi.git
 chezmoi apply
-sudo apt update && sudo apt install libnss3 libatk-bridge2.0-0 libatk1.0-0 libgrk-3-0 libgbm1 libasound2 
+sudo apt update && sudo apt install -y libnss3 libatk-bridge2.0-0 libatk1.0-0 libgtk-3-0 libgbm1 libasound2 
